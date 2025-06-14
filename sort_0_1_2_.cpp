d6f20f1d67509c030de48c435500d60f1bf90ef5
gtushar8055 <tushargupta12312021@gmail.com>
2025-06-14 12:42:35 +0530

    Day10

---
+++ sort_0_1_2_.cpp
@@ -0,0 +1,32 @@
+// Day10 --> 14/06/2025
+// Sorts 0s , 1s , 2s --> GfGs
+
+class Solution {
+  public:
+    void merge(vector<int>& a, vector<int>& b, vector<int>& res) {
+        int i = 0, j = 0, k = 0;
+        while (i < a.size() && j < b.size()) {
+            if (a[i] < b[j]) res[k++] = a[i++];
+            else res[k++] = b[j++];
+        }
+        while (i < a.size()) res[k++] = a[i++];
+        while (j < b.size()) res[k++] = b[j++];
+    }
+
+    void mergeSort(vector<int>& v) {
+        int n = v.size();
+        if (n == 1) return;
+        int n1 = n / 2, n2 = n - n1;
+        vector<int> a(n1), b(n2);
+        for (int i = 0; i < n1; i++) a[i] = v[i];
+        for (int i = 0; i < n2; i++) b[i] = v[i + n1];
+
+        mergeSort(a);
+        mergeSort(b);
+        merge(a, b, v);
+    }
+    
+    void sort012(vector<int>& arr) {
+        mergeSort(arr);
+    }
+};
